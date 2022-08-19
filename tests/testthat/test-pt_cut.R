0697bfa4df08fe2c67f46b46e5c17bf2637ac78f
Harris, Alana {MDBB~Welwyn} <alana.harris@roche.com>
2022-08-19 18:02:21 +0200

    Updating namespace and refreshing docs

--- tests/testthat/test-pt_cut.R
+++ tests/testthat/test-pt_cut.R
@@ -1,6 +1,6 @@
 # Test 1 - one patient not in DCUT is flagged to be removed
 
-input_dm <- tibble::tribble(
+input_ae <- tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC,
   "my_study", "subject1", 1, "2020-01-02",
   "my_study", "subject1", 2, "2020-08-31",
@@ -10,7 +10,7 @@ input_dm <- tibble::tribble(
   "my_study", "subject4", 1, "2020-11-02"
 )
 
-input_dcut <- tibble::tribble(
+input_dcut <- tribble(
   ~STUDYID, ~USUBJID, ~DCUTDT,
   "my_study", "subject1", "2020-02-20",
   "my_study", "subject2", "2020-02-20",
@@ -18,7 +18,7 @@ input_dcut <- tibble::tribble(
 )
 
 
-expected_dm <- tibble::tribble(
+expected_ae <- tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~DCUT_TEMP_REMOVE,
   "my_study", "subject1", 1, "2020-01-02", "",
   "my_study", "subject1", 2, "2020-08-31", "",
@@ -30,24 +30,23 @@ expected_dm <- tibble::tribble(
 
 test_that("The one patient not in DCUT is flagged to be removed", {
   expect_equal(
-    pt_cut(dataset_sdtm = input_dm,
-           dataset_cut = input_dcut,
-           cut_var = DCUTDT),
-    expected_dm
+    pt_cut(dataset_sdtm = input_ae,
+           dataset_cut = input_dcut),
+    expected_ae
   )
 })
 
 
 # Test 2 - multiple patients not in DCUT are flagged to be removed
 
-input_dcut2 <- tibble::tribble(
+input_dcut2 <- tribble(
   ~STUDYID, ~USUBJID, ~DCUTDT,
   "my_study", "subject2", "2020-02-20",
   "my_study", "subject4", "2020-02-20"
 )
 
 
-expected_dm2 <- tibble::tribble(
+expected_ae2 <- tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~DCUT_TEMP_REMOVE,
   "my_study", "subject1", 1, "2020-01-02", "Y",
   "my_study", "subject1", 2, "2020-08-31", "Y",
@@ -59,24 +58,23 @@ expected_dm2 <- tibble::tribble(
 
 test_that("The multiple patients not in DCUT are flagged to be removed", {
   expect_equal(
-    pt_cut(dataset_sdtm = input_dm,
-           dataset_cut = input_dcut2,
-           cut_var = DCUTDT),
-    expected_dm2
+    pt_cut(dataset_sdtm = input_ae,
+           dataset_cut = input_dcut2),
+    expected_ae2
   )
 })
 
 
-# Test 3 - no patients match in DCUT
+# Test 3 - no patients match in DCUT, all records should be flagged
 
-input_dcut3 <- tibble::tribble(
+input_dcut3 <- tribble(
   ~STUDYID, ~USUBJID, ~DCUTDT,
   "my_study", "subject5", "2020-02-20",
   "my_study", "subject6", "2020-02-20",
 )
 
 
-expected_dm3 <- tibble::tribble(
+expected_ae3 <- tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~DCUT_TEMP_REMOVE,
   "my_study", "subject1", 1, "2020-01-02", "Y",
   "my_study", "subject1", 2, "2020-08-31", "Y",
@@ -88,17 +86,16 @@ expected_dm3 <- tibble::tribble(
 
 test_that("If no patients in DCUT match the dataset then all records are flagged to be removed", {
   expect_equal(
-    pt_cut(dataset_sdtm = input_dm,
-           dataset_cut = input_dcut3,
-           cut_var = DCUTDT),
-    expected_dm3
+    pt_cut(dataset_sdtm = input_ae,
+           dataset_cut = input_dcut3),
+    expected_ae3
   )
 })
 
 
-# Test 4 - all patients in DCUT are in dataset
+# Test 4 - all patients in DCUT are in dataset, no records should be flagged
 
-input_dcut4 <- tibble::tribble(
+input_dcut4 <- tribble(
   ~STUDYID, ~USUBJID, ~DCUTDT,
   "my_study", "subject1", "2020-02-20",
   "my_study", "subject2", "2020-02-20",
@@ -107,7 +104,7 @@ input_dcut4 <- tibble::tribble(
 )
 
 
-expected_dm4 <- tibble::tribble(
+expected_ae4 <- tribble(
   ~STUDYID, ~USUBJID, ~AESEQ, ~AESTDTC, ~DCUT_TEMP_REMOVE,
   "my_study", "subject1", 1, "2020-01-02", "",
   "my_study", "subject1", 2, "2020-08-31", "",
@@ -119,10 +116,9 @@ expected_dm4 <- tibble::tribble(
 
 test_that("If all patients in DCUT match the dataset then no records are flagged to be removed", {
   expect_equal(
-    pt_cut(dataset_sdtm = input_dm,
-           dataset_cut = input_dcut3,
-           cut_var = DCUTDT),
-    expected_dm3
+    pt_cut(dataset_sdtm = input_ae,
+           dataset_cut = input_dcut4),
+    expected_ae4
   )
 })
 
