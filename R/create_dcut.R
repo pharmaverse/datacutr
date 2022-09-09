771b58b09efbfe69177e9c41eaa8d6bca9c121fe
Harris, Alana {MDBB~Welwyn} <alana.harris@roche.com>
2022-09-09 17:30:00 +0200

    Adding initial draft of create_dcut function

---
+++ R/create_dcut.R
@@ -0,0 +1,54 @@
+#' Create DCUT dataset
+#'
+#' Use to create a datacut dataset containing the variables `USUBJID`, `DCUTDTC`, `DCUTDT`, `DCUTDESC`.
+#'
+#' @param dataset_ds Input DS SDTMv dataset
+#' @param filter Condition to filter patients in DS, should give 1 row per patient
+#' @param cut_date Datacut date, e.g. "2022-10-22"
+#'
+#' @author Alana Harris
+#'
+#' @return Datacut dataset
+#'
+#' @export
+#'
+#' @keywords derive
+#'
+#' @examples
+#'library(admiral)
+#'ds <- tibble::tribble(
+#'  ~USUBJID, ~DSSEQ, ~DSDECOD, ~DSSDTC,
+#'  "subject1", 1, "INFORMED CONSENT",      "2020-06-23",
+#'  "subject1", 2, "RANDOMIZATION",         "2020-08-22",
+#'  "subject1", 3, "WITHDRAWAL BY SUBJECT", "2020-05-01",
+#'  "subject2", 1, "INFORMED CONSENT",      "2020-07-13",
+#'  "subject3", 1, "INFORMED CONSENT",      "2020-06-03",
+#'  "subject4", 1, "INFORMED CONSENT",      "2021-01-01",
+#'  "subject4", 2, "RANDOMIZATION",         "2023-01-01"
+#')
+#'
+#'temp_ds <- impute_sdtm(dsin=ds, varin=DSSDTC, varout=DCUT_TEMP_DSSDTC)
+#'
+#'dcut <- create_dcut(dataset_ds = temp_ds,
+#'                    filter = DSDECOD == "RANDOMIZATION" & DCUTDT>=DCUT_TEMP_DSSDTC,
+#'                    cut_date = "2022-01-01",
+#'                    cut_description = "Clinical Cutoff Date")
+
+create_dcut <- function(dataset_ds,
+                        filter,
+                        cut_date,
+                        cut_description) {
+
+  assert_data_frame(dataset_ds,
+                    required_vars = quo_c(vars(USUBJID)))
+
+  filter <- assert_filter_cond(enquo(filter), optional = TRUE)
+
+  dataset <- dataset_ds %>%
+    mutate(DCUTDTC = cut_date) %>%
+    mutate(DCUTDESC = cut_description) %>%
+    impute_dcutdtc(dsin=., varin=DCUTDTC, varout=DCUTDT) %>%
+    filter_if(filter) %>%
+    subset(select = c(USUBJID, DCUTDTC, DCUTDT, DCUTDESC))
+  dataset
+}
