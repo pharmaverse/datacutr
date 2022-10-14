233ba9f777e56ad46d2a615a0104df3d0b0b9154
Rees, Nathan {MDBL~Welwyn} <nathan.rees@roche.com>
2022-10-14 18:41:18 +0200

    Updated the process_cut function based on Kieran's updates to the template script.

--- R/datcutr-package.R
+++ R/datcutr-package.R
@@ -1,8 +1,8 @@
 #' @keywords internal
-#' @importFrom dplyr case_when filter mutate select starts_with inner_join anti_join vars left_join
+#' @importFrom dplyr case_when filter mutate select starts_with inner_join anti_join vars left_join syms
 #' @importFrom magrittr %>%
 #' @importFrom rlang := enquo quo_name !! is_quosure quo_is_null as_quosures
-#' @importFrom purrr map_lgl
+#' @importFrom purrr map_lgl pmap map
 #' @importFrom lubridate ymd_hms
 #' @importFrom admiraldev assert_symbol assert_data_frame assert_character_scalar assert_filter_cond filter_if quo_c
 #' @importFrom assertthat assert_that
