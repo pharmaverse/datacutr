06ace51d6bd403ac9b1aa82885984351eedfe727
Rees, Nathan {MDBL~Welwyn} <nathan.rees@roche.com>
2023-03-17 18:56:21 +0100

    Updated drop_temp_vars function for QC comments

--- R/drop_temp_vars.R
+++ R/drop_temp_vars.R
@@ -2,11 +2,13 @@
 #'
 #' @description Drops all the temporary variables (variables beginning with TEMP_) from the input
 #' dataset. Also allows the user to specify whether or not to drop the temporary variables needed
-#' throughout the datacut process (variables beginning with DCUT_TEMP_).
+#' throughout multiple steps of the datacut process (variables beginning with DCUT_TEMP_).
 #'
-#' @details The idea is that, when used throughout the datacut process, drop_dcut_temp would
-#' always be set to FALSE until the very end of the datacut process, in which case,
-#' drop_dcut_temp would be set to TRUE.
+#' @details The other functions within this package use `drop_temp_vars` with the `drop_dcut_temp`
+#' argument set to FALSE so that the variables needed across multiple steps of the process are
+#' kept. The final datacut takes place in the `apply_cut` function, at which point `drop_temp_vars`
+#' is used with the `drop_dcut_temp` argument set to TRUE, so that all temporary variables are
+#' dropped.
 #'
 #' @param dsin Name of input dataframe
 #' @param drop_dcut_temp Whether or not to drop variables beginning with DCUT_TEMP_ (TRUE/FALSE).
@@ -18,10 +20,19 @@
 #' @keywords user_utility
 #'
 #' @examples
-#' test <- data.frame(USUBJID = c(""), TEMP_XYZ = c(""), DCUT_TEMP_XYZ = c(""))
-#' drop_temp_vars(dsin = test) # Drops temp_ and dcut_temp_ variables
-#' drop_temp_vars(dsin = test, drop_dcut_temp = TRUE) # Drops temp_ and dcut_temp_ variables
-#' drop_temp_vars(dsin = test, drop_dcut_temp = FALSE) # Drops temp_ variables
+#' ae <- tibble::tribble(
+#'   ~USUBJID, ~AESEQ, ~TEMP_FLAG, ~DCUT_TEMP_REMOVE,
+#'   "subject1", 1, "Y", NA,
+#'   "subject1", 2, "Y", NA,
+#'   "subject1", 3, NA, "Y",
+#'   "subject2", 2, "Y", NA,
+#'   "subject3", 1, NA, "Y",
+#'   "subject4", 1, NA, "Y"
+#' )
+#' drop_temp_vars(dsin = ae) # Drops temp_ and dcut_temp_ variables
+#' drop_temp_vars(dsin = ae, drop_dcut_temp = TRUE) # Drops temp_ and dcut_temp_ variables
+#' drop_temp_vars(dsin = ae, drop_dcut_temp = FALSE) # Drops temp_ variables
+#'
 drop_temp_vars <- function(dsin, drop_dcut_temp = TRUE) {
   # Check if dataframe exists and that drop_dcut_temp is true or false
   assert_data_frame(dsin)
