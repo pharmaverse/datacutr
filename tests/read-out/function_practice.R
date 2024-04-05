49510a23a6ae23b90edf43c57d5557143fd37a71
Cara Andrews <cara.andrews@roche.com>
2024-04-05 16:39:30 +0000

    basic Rmarkdown file produced & exploratory scripts

--- tests/read-out/function_practice.R
+++ tests/read-out/function_practice.R
@@ -41,3 +41,46 @@ dm_cut <- special_dm_cut(
 )
 
 dm_cut
+
+## Summary Table ----
+
+df_tabs <- function(data){
+  cyc_num <- 1
+  for (df in data){
+    name <- names(data)[cyc_num]
+    table <- data[cyc_num]
+    cat("####", name, " \n")
+    print(knitr::kable(table))
+    cat(' \n\n')
+    cyc_num <- cyc_num + 1
+  }
+}
+
+summary <- function(data){
+  cyc_num <- 1
+  for (df in data){
+    name <- names(data)[cyc_num]
+    print(df)
+    x <- length(which(df$DCUT_TEMP_REMOVE == "Y"))
+    print(paste("Number of records removed in ", name, " is ", x))
+    cyc_num <- cyc_num + 1
+  }
+}
+
+
+summary(patient_cut_data)
+
+sep_dfs(patient_cut_data)
+
+patient_cut_data$sc
+
+tb <- patient_cut_data[1]
+tb2 <- data.frame(tb)
+x <- length(which(patient_cut_data$sc$DCUT_TEMP_REMOVE == "Y"))
+x <- length(which(dm_cut$DCUT_TEMP_REMOVE == "Y"))
+
+patient_cut_data[name]
+patient_cut_data$sc
+
+patient_cut_data[i]
+patient_cut_data["sc"]
